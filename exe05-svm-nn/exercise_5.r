d543c6384ad2f7c44c83419edf1d311d95ec99b1
dueruen <32197042+dueruen@users.noreply.github.com>
2021-04-28 17:38:38 +0200

    Started on 5.1.1

--- exe05-svm-nn/exercise_5.r
+++ exe05-svm-nn/exercise_5.r
@@ -16,9 +16,21 @@ items_per_person = nrow(id) / length(idList)
 id_train <- id[1:(items_per_person*9),]
 id_test <- id[(items_per_person*9 + 1):(items_per_person*13),]
 
+#####################
+## Exercise 5.1 - SVM
+#####################
+
 #########
-## 
+## 5.1.1
 #########
 
-classifier_rbf <- ksvm(V1~ ., data = id_train, kernel = "rbfdot", kpar=list(sigma=0.05), C = 1)
+start_time <- Sys.time()
+classifier_rbf <- ksvm(V1~ ., data = id_train, kernel = "polydot", kpar="automatic", C = 1)
+end_time <- Sys.time()
+run_time <- end_time - start_time
 
+classifier_pre <- predict(classifier_rbf,id_test)
+
+cf <- confusionMatrix(classifier_pre, id_test[,1])
+print( sum(diag(cf$table))/sum(cf$table) )
+print( run_time )
