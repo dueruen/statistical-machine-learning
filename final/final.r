e48cd07256f3060990747612316c594e140467e6
dueruen <32197042+dueruen@users.noreply.github.com>
2021-05-21 10:45:33 +0200

    Start on final

---
+++ final/final.r
@@ -0,0 +1,18 @@
+library(rpart)
+library(rpart.plot) #SKAL INSTALLERES
+library(stats)
+library(randomForest)
+library(caret)
+library(kernlab)
+library(RSNNS)
+
+#Load the dataset
+load("idList-cornered-100-2021.Rdata")
+
+id <- do.call(rbind, idList[])
+id <- as.data.frame(id)
+id[,1] <- factor(id[,1])
+
+items_per_person = nrow(id) / length(idList)
+id_train <- id[1:(items_per_person*9),]
+id_test <- id[(items_per_person*9 + 1):(items_per_person*13),]
