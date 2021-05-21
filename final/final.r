d7e24bc88cb7b71e901dd22d4b587d5e3b07495a
dueruen <32197042+dueruen@users.noreply.github.com>
2021-05-21 10:55:21 +0200

    Get data

--- final/final.r
+++ final/final.r
@@ -7,12 +7,16 @@ library(kernlab)
 library(RSNNS)
 
 #Load the dataset
-load("idList-cornered-100-2021.Rdata")
+load("idList-FinalExam.Rdata")
 
 id <- do.call(rbind, idList[])
 id <- as.data.frame(id)
 id[,1] <- factor(id[,1])
 
 items_per_person = nrow(id) / length(idList)
-id_train <- id[1:(items_per_person*9),]
-id_test <- id[(items_per_person*9 + 1):(items_per_person*13),]
+disjunct_train <- id[1:(items_per_person*30),]
+disjunct_test <- id[(items_per_person*30 + 1):(items_per_person*38),]
+
+dataset_shuffle <-id[sample(nrow(id)),]
+all_persons_train <- dataset_shuffle[1:(items_per_person*30),]
+all_persons_test <- dataset_shuffle[(items_per_person*30 + 1):(items_per_person*38),]
